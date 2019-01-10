c4b4b4f27e2ab30db78f1fa736133271fb01c6fa
thedon122 <donovan122@yahoo.com>
2019-01-09 19:31:39 -0500

    Updates

---
+++ precache-manifest.beea401458fea0cb4999a7f7666eb2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "642ec1baa35e07bfa96a",
+    "url": "/static/css/main.07dfb4cf.chunk.css"
+  },
+  {
+    "revision": "642ec1baa35e07bfa96a",
+    "url": "/static/js/main.642ec1ba.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "207ae33fec09334db781110fb09de80d",
+    "url": "/index.html"
+  }
+];
